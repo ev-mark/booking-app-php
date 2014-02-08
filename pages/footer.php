cc86566ed7e913ab95da779870932b654db1fa1a
imeremi <imeremi@users.noreply.github.com>
2014-02-08 02:02:39 +0100

    Norsk oversettelse footer.php

--- pages/footer.php
+++ pages/footer.php
@@ -1,11 +1,11 @@
 			<div id="footer_out" >
 				<div id="footer" >
 					<div class="footer_left_text">
-						<p><b>Taxi Cars</b><br />The Street<br />London<br />N1 1AA</p>
+						<p><b>Firmanavn</b><br />Gateadresse<br />Poststed<br />N1 1AA</p>
 					</div>
-					<div class="footer_right_text">
-						<b>Telephone Bookings</b><p>020 000 0000</p>
-						<b>General Enquiries</b><p>020 111 1111</p>
+					<div class="footer_right_tet">
+						<b>Telefon Booking</b><p>776 00 000</p>
+						<b>Andre henvendelser</b><p>776 00 001</p>
 					</div>
 				</div>
 			</div>
